5ee12934906c465df628dbfd385b2a96490b0333
Bruno Moreno <brunobrouss@gmail.com>
2024-10-29 03:51:03 -0600

    actualizacion de calculadora, el dinero se reinvierte cada dia

--- script-calculo-rendimiento.js
+++ script-calculo-rendimiento.js
@@ -2,19 +2,26 @@ document.getElementById('calcForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Evita que el formulario se envíe y la página se recargue.
 
     // Obtén los valores de los inputs.
-    const dato1 = parseFloat(document.getElementById('interes').value);     //interes
-    const dato2 = parseFloat(document.getElementById('plazo').value);   //plazo
-    const dato3 = parseFloat(document.getElementById('monto').value);   //monto
+    const interes = parseFloat(document.getElementById('interes').value);     //interes
+    const plazo = parseFloat(document.getElementById('plazo').value);   //plazo
+    const monto = parseFloat(document.getElementById('monto').value);   //monto
 
     // Realiza un cálculo con los tres datos (ejemplo: suma de los tres).
-    let aux = dato1 * .01;
-    let aux1 = (dato3 * aux)/365;
-    let aux2 = (aux1 * dato2) + dato3;
-    let aux3 = aux1 * dato2;
+    // let aux = interes * .01;
+    // let aux1 = (monto * aux)/365;
+    // let aux2 = (aux1 * plazo) + monto;
+    // let aux3 = aux1 * plazo;
+    let montoVar = monto;
+    let gananciaTotal;
+    let interesDia = 1 + (.01 * (interes/365));
 
-    // const resultado = dato1 + dato2 + dato3;
+    for (var i = 0; i < plazo; i++) {
+        montoVar = montoVar * interesDia;
+      }
+    gananciaTotal = montoVar - monto;
+    // const resultado = interes + plazo + monto;
 
     // Muestra el resultado en el div de resultado.
-    document.getElementById('resultadototal').textContent = `${aux2.toFixed(2)}`;
-    document.getElementById('resultadoganancia').textContent = `${aux3.toFixed(2)}`;
+    document.getElementById('resultadototal').textContent = `${montoVar.toFixed(2)}`;
+    document.getElementById('resultadoganancia').textContent = `${gananciaTotal.toFixed(2)}`;
 });
